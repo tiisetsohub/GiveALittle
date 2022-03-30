08d64b05088356c0d5557da3f79e76ea90523832
tiisetso <tiimojalefa@gmail.com>
2022-03-30 21:12:35 +0200

    Initial commit

---
+++ givealittle/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import './App.css';
+import Demo from './Pages/Demo'
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+             <Route path="/demo" element={<Demo />} />    //when you want to test your page, replace the demo with your page details here. As soon as you are done, RESTORE IT TO THE STATE IN WHICH YOU FOUND IT
+        </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ givealittle/src/Pages/Demo.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function  Demo(){
+    return (
+        <p>HELLO!</p>
+    )
+}
+//this page is just a demo to help you create your page, DO NOT EDIT OR DELETE IT!!!!
