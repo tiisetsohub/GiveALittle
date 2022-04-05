725d87ffec7c3e40042b274886b08132c09f65ee
tiisetso <tiimojalefa@gmail.com>
2022-04-06 01:00:16 +0200

    database added

--- givealittle/src/App.js
+++ givealittle/src/App.js
@@ -1,7 +1,7 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import './App.css';
 import Demo from './Pages/Demo'   //copy with your page name to call it
-import { db } from './firebase-config'
+
 
 function App() {
   return (
