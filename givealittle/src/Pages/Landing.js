f74a47c1e00c99b7b8bd8d373c0ad4b2ae718e0e
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2022-04-26 01:39:40 +0200

    home and landing page inconsistent sytling fix

--- givealittle/src/Pages/Landing.js
+++ givealittle/src/Pages/Landing.js
@@ -129,7 +129,7 @@ export default function Landing() {
                     <button className="btnclose" onClick={() => setShow(false)}>close</button>
 
                     <div>
-                        <img src={item.Image} />
+                        <img style={{boxShadow: "0px 0px 10px 0px rgb(200, 200, 200)"}} src={item.Image} />
                     </div>
                     
                     <h3>{item.Name}</h3>
