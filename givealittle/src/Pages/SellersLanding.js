5ff19b2bf4b1987e0cca2d91f7c2e2204c2bf6fd
Brianhulela <101281239+Brianhulela@users.noreply.github.com>
2022-05-01 19:13:14 +0200

    added seller details to top of page

--- givealittle/src/Pages/SellersLanding.js
+++ givealittle/src/Pages/SellersLanding.js
@@ -7,6 +7,7 @@ import { useEffect, useState, useContext } from 'react';
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { db } from '../firebase-config';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SellerDetails from '../components/SellerDetails';
 
 // To get current user imports
 import { NameContext } from '../Context';
@@ -15,6 +16,7 @@ import {
   Container, Row, Col
 } from 'reactstrap';
 import { applyActionCode } from 'firebase/auth';
+import { async } from '@firebase/util';
 
 function SellersLanding() {
 
@@ -22,6 +24,7 @@ function SellersLanding() {
   const [Inventory, setItems] = useState([]);           //state for inventory
   const itemRef = collection(db, "Inventory");            //reference to inventory in database
 
+  const [Users, setUsers] = useState([]);
 
   useEffect(() => {       //loads data from database
     const getItems = async () => {
@@ -32,6 +35,16 @@ function SellersLanding() {
     getItems()
 }, []);
   
+useEffect(() => {
+  const getUsers = async () => {
+    const data = await getDocs(collection(db, "Users"));
+    setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  }
+  getUsers()
+}, []);
+
+
+
   
   return (
     <div className='body'>
@@ -39,15 +52,21 @@ function SellersLanding() {
       <Navbar/>
       <div style={{textAlign: "center"}}>
 
+      <div>
+        {Users.map((user, idx) => (
+       user.Email == name
+        ? (
+          <SellerDetails Name={user.Name} Email={user.Email} Cell={user.Cell}/>
+        )
+        : null
+        ))}
+      </div>
 
       <Link to="/sell">
         <button className='add-product-button'>Add Product</button>
       </Link>
-
-      <h3>{name}</h3>
         
       </div> 
-
 
       <div className='products-container'>
       <ul>
