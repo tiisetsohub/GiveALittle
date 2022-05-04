22bb7caeb3e2f620fed0016c6b32dc1cb11d0451
KgaugeloNalane <101181216+KgaugeloNalane@users.noreply.github.com>
2022-05-04 20:38:57 +0200

    Add files via upload

--- givealittle/src/Pages/Sell.js
+++ givealittle/src/Pages/Sell.js
@@ -1,23 +1,30 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { db, storage } from '../firebase-config';
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { useState, useEffect, useContext } from 'react';
+import { db } from '../firebase-config';
 import { collection, getDocs, addDoc } from "firebase/firestore";
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './Login.css';
+import { NameContext } from '../Context';
 
 export default function Sell() {
     const [newName, setNewName] = useState("");     //state for item name
     const [newDescription, setNewDesnewDescription] = useState("");         //state for description 
+    const [newImg1, setnewImg1] = useState("");                   //state for image
+    const [newImg2, setnewImg2] = useState(""); 
+    const [newImg3, setnewImg3] = useState(""); 
     const [newPrice, setNewPrice] = useState(0);                    //state for price
     const [newQuantity, setNewQuantity] = useState(0);              //state for quantity
 
+    const {name} = useContext(NameContext);       //state for currently signed in user email
+
     const [item, setItem] = useState([]);           //state for item
-    const itemRef = collection(db, "Inventory");            //refernce for item
+    const itemRef = collection(db, "SellItems");            //refernce for item
+
 
     const addItem = async () => {           //handles adding an item to database
-        await addDoc(itemRef, { Name: newName, Description: newDescription, Price: newPrice, Quantity: newQuantity, Image1: url1, Image2: url2, Image3: url3 });
-        alert("Added")
+        await addDoc(itemRef, {Name: newName, Description: newDescription, Price: newPrice, Quantity: newQuantity, Image1: newImg1, Image2: newImg2, Image3: newImg3, Seller: name });
+        alert("Added") 
+        
     }
 
     useEffect(() => {           //gets data from database
@@ -29,80 +36,35 @@ export default function Sell() {
         getItems()
     }, []);
 
-    
-    const [image1, newImg1]=useState("");   
-    const [image2, newImg2]=useState(""); 
-    const [image3, newImg3]=useState("");         //state for image
-    const [url1, setUrl1]=useState("");
-    const [url2, setUrl2]=useState("");
-    const [url3, setUrl3]=useState("");
-    
-    const handle1stUpload = async ()=>{
-        if(image1==null) {
-            return;
-        }
-        else{
-            const imageRef = ref(storage, `imagefolder/${image1.name}`);
-            uploadBytes(imageRef, image1).then(()=>{
-                alert("image uploaded")
-            })
-            await getDownloadURL(imageRef).then((x)=> {
-                setUrl1(x);
-            })
-
-        
-    }
-          
-    };
-    const handle2ndUpload = async ()=>{
-        if(image2==null) {
-            return;
-        }
-        else{
-            const imageRef = ref(storage, `imagefolder/${image2.name}`);
-            uploadBytes(imageRef, image2).then(()=>{
-                alert("image uploaded")
-            })
-            await getDownloadURL(imageRef).then((x)=> {
-                setUrl2(x);
-            })
-
-        
-    }
-          
-    };
-    const handle3rdUpload = async ()=>{
-        if(image3==null) {
-            return;
-        }
-        else{
-            const imageRef = ref(storage, `imagefolder/${image3.name}`);
-            uploadBytes(imageRef, image3).then(()=>{
-                alert("image uploaded")
-            })
-            await getDownloadURL(imageRef).then((x)=> {
-                setUrl3(x);
-            })
-
-        
-    }
-          
-    };
 
     return (
         <div className="bigdiv">
             <Navbar />
             <h1>Add Item</h1>
             <div className='logindiv'>          {/*form for item information*/}
-                <input className="edtname" id="input" placeholder="Name" onChange={(event) => {
+                <input className="edtname" id="input" placeholder="Item Name" onChange={(event) => {
                     setNewName(event.target.value)
                 }} />
                 <br />
-                <input className="edtdesc" id="input" placeholder="Description" onChange={(event) => {
+                <input className="edtdesc" id="input" placeholder="Item Description" onChange={(event) => {
                     setNewDesnewDescription(event.target.value)
                 }} />
-               <br />
-                <input className="edtprice" type="number" id="input" placeholder="Price" min="19.99" onChange={(event) => {
+                <br />
+                <input className="edtimg1" id="input" placeholder="Image Link" onChange={(event) => {
+                    setnewImg1(event.target.value)
+                }} />
+                <br />
+                <br />
+                <input className="edtimg2" id="input" placeholder="Image Link" onChange={(event) => {
+                    setnewImg2(event.target.value)
+                }} />
+                <br />
+                <br />
+                <input className="edtimg3" id="input" placeholder="Image Link" onChange={(event) => {
+                    setnewImg3(event.target.value)
+                }} />
+                <br />
+                <input className="edtprice" type="number" id="input" placeholder="Item Price" min="19.99" onChange={(event) => {
                     let t = parseFloat(event.target.value)
                     setNewPrice(t)
                 }} />
@@ -112,20 +74,9 @@ export default function Sell() {
                     setNewQuantity(t)
                 }} />
                 <br />
-                <br />
-                <input type="file" id="input" onChange={(event)=>{newImg1(event.target.files[0])}} />
-                <button className="btnimg" onClick={handle1stUpload}>Upload</button>
-                <br />
-                <br />
-                <input type="file" id="input" onChange={(event)=>{newImg2(event.target.files[0])}} />
-                <button className="btnimg" onClick={handle2ndUpload}>Upload</button>
-                <br />
-                <br />
-                <input type="file" id="input" onChange={(event)=>{newImg3(event.target.files[0])}} />
-                <button className="btnimg" onClick={handle3rdUpload}>Upload</button>
-                <br />
-
-                <button className="btnadd" id="btn" onClick={addItem}>Add</button>
+                <Link to="/sellerslanding">
+                    <button className="btnadd" id="btn" onClick={addItem}>Add</button>
+                </Link>
             </div>
         </div>
     )
