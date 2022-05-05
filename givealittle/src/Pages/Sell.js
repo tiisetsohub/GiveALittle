021ec0c7762c28d6f2c0636256e4fa78bbd0ead4
tiisetso <tiimojalefa@gmail.com>
2022-05-06 01:10:42 +0200

    merged review system

--- givealittle/src/Pages/Sell.js
+++ givealittle/src/Pages/Sell.js
@@ -15,9 +15,10 @@ export default function Sell() {
   const [newPrice, setNewPrice] = useState(0); //state for price
   const [newQuantity, setNewQuantity] = useState(0); //state for quantity
 
-<<<<<<< HEAD
   const [item, setItem] = useState([]); //state for item
   const itemRef = collection(db, "Inventory"); //refernce for item
+  const { name } = useContext(NameContext);       //state for currently signed in user email
+  const [Specs, setSpecs] = useState([]);     //State for the specs
 
   const addItem = async () => {
     //handles adding an item to database
@@ -29,6 +30,8 @@ export default function Sell() {
       Image: newImg,
       Stars: "",
       Review: "",
+      Seller: name,
+      Specs: Specs 
     });
     alert("Added");
   };
@@ -47,134 +50,43 @@ export default function Sell() {
     <div className="bigdiv">
       <Navbar />
       <h1>Add Item</h1>
-      <div className="logindiv">
-        {" "}
-        {/*form for item information*/}
-        <input
-          className="edtname"
-          id="input"
-          placeholder="Item Name"
-          onChange={(event) => {
-            setNewName(event.target.value);
-          }}
-        />
+      <div className='logindivb'>          {/*form for item information*/}
+        <input className="edtname" id="input" placeholder="Item Name" onChange={(event) => {
+          setNewName(event.target.value)
+        }} />
         <br />
-        <input
-          className="edtdesc"
-          id="input"
-          placeholder="Item Description"
-          onChange={(event) => {
-            setNewDesnewDescription(event.target.value);
-          }}
-        />
+        <input className="edtdesc" id="input" placeholder="Item Description" onChange={(event) => {
+          setNewDesnewDescription(event.target.value)
+        }} />
         <br />
-        <input
-          className="edtimg"
-          id="input"
-          placeholder="Image Link"
-          onChange={(event) => {
-            setnewImg(event.target.value);
-          }}
-        />
+        <input className="edtimg" id="input" placeholder="Image Link" onChange={(event) => {
+          setnewImg(event.target.value)
+        }} />
         <br />
-        <input
-          className="edtprice"
-          type="number"
-          id="input"
-          placeholder="Item Price"
-          min="19.99"
-          onChange={(event) => {
-            let t = parseFloat(event.target.value);
-            setNewPrice(t);
-          }}
-        />
+        <input className="edtprice" type="number" id="input" placeholder="Item Price" min="19.99" onChange={(event) => {
+          let t = parseFloat(event.target.value)
+          setNewPrice(t)
+        }} />
         <br />
-        <input
-          type="number"
-          className="edtquant"
-          id="input"
-          placeholder="Quantity"
-          min="1"
-          max="100"
-          onChange={(event) => {
-            let t = parseInt(event.target.value);
-            setNewQuantity(t);
-          }}
-        />
+        <input type="number" className="edtquant" id="input" placeholder="Quantity" min="1" max="100" onChange={(event) => {
+          let t = parseInt(event.target.value)
+          setNewQuantity(t)
+        }} />
         <br />
-        <button className="btnadd" id="btn" onClick={addItem}>
-          Add
-        </button>
+
+        <h4>Add Product Specifications</h4>
+        <SpecsPage Specs={Specs} setSpecs={setSpecs} />
+
+        <Link to="/sellerslanding">
+          <button className="btnadd" id="btn" onClick={addItem}>Add</button>
+        </Link>
+
+
+
+
       </div>
     </div>
-  );
-=======
-    const {name} = useContext(NameContext);       //state for currently signed in user email
-
-    const [item, setItem] = useState([]);           //state for item
-    const itemRef = collection(db, "Inventory");            //refernce for item
-
-
-    const [Specs, setSpecs] = useState([]);     //State for the specs
-
-
-    const addItem = async () => {           //handles adding an item to database
-        await addDoc(itemRef, {Name: newName, Description: newDescription, Price: newPrice, Quantity: newQuantity, Image: newImg, Seller: name, Specs: Specs });
-        alert("Added") 
-        
-    }
-
-    useEffect(() => {           //gets data from database
-        const getItems = async () => {
-            const data = await getDocs(itemRef);
-            setItem(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-        }
-        getItems()
-    }, []);
-
-
-    return (
-        <div className="bigdiv">
-            <Navbar />
-            <h1>Add Item</h1>
-            <div className='logindivb'>          {/*form for item information*/}
-                <input className="edtname" id="input" placeholder="Item Name" onChange={(event) => {
-                    setNewName(event.target.value)
-                }} />
-                <br />
-                <input className="edtdesc" id="input" placeholder="Item Description" onChange={(event) => {
-                    setNewDesnewDescription(event.target.value)
-                }} />
-                <br />
-                <input className="edtimg" id="input" placeholder="Image Link" onChange={(event) => {
-                    setnewImg(event.target.value)
-                }} />
-                <br />
-                <input className="edtprice" type="number" id="input" placeholder="Item Price" min="19.99" onChange={(event) => {
-                    let t = parseFloat(event.target.value)
-                    setNewPrice(t)
-                }} />
-                <br />
-                <input type="number" className="edtquant" id="input" placeholder="Quantity" min="1" max="100" onChange={(event) => {
-                    let t = parseInt(event.target.value)
-                    setNewQuantity(t)
-                }} />
-                <br />
-
-                <h4>Add Product Specifications</h4>
-                <SpecsPage Specs={Specs} setSpecs={setSpecs}/>
-
-                <Link to="/sellerslanding">
-                    <button className="btnadd" id="btn" onClick={addItem}>Add</button>
-                </Link>
-
-                
-                
-                
-            </div>
-        </div>
-    )
->>>>>>> origin/main
+  )
 }
 
 function Navbar() {
