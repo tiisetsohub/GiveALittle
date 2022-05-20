f247d5a0450bafc18d6c3ec1b29282aace020700
MotheoTee <101192548+MotheoTee@users.noreply.github.com>
2022-05-20 18:32:55 +0200

    About

--- givealittle/src/Pages/About.js
+++ givealittle/src/Pages/About.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import HomeNavBar from '../components/HomeNavBar';
+import Navigation from '../components/Navigation';
 import { NameContext, LoginContext, CartContext } from '../Context'
 
 export default function About() {
@@ -8,9 +10,17 @@ export default function About() {
 
     return (
         <div>
+            <Navigation/>
             <h3 className='about_details'>
-                This Is About Page, 
+                About Us 
             </h3>
+            <br/>
+            <h6>
+                GiveALittle is an Online MarketPlace similar to takealot for both Buyers
+                and Sellers which allows
+                them to buy products or goods they need online OR sell the items they want to sell,
+                meaning they can be both a buyer and the seller at the same time.
+            </h6>
         </div>
     )
 }
