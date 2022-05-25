b02ea3910bb776f677c38bcb486f1f529593749c
MotheoTee <101192548+MotheoTee@users.noreply.github.com>
2022-05-25 13:11:13 +0200

    just fixing the home navbar

--- givealittle/src/Pages/Home.js
+++ givealittle/src/Pages/Home.js
@@ -176,13 +176,17 @@ export default function Home() {
         <div className="navbar">
           <div className="leftside">
             <div className="links" id={showLinks ? "hidden" : ""}>
+              <Link className="navlink" to='/'>
+                <p>Home</p>
+              </Link>
+
               <Link className="navlink" to="/login">
                 <p>Login/Signup</p>
               </Link>
-              <Link className="navlink" to="/about">
+              <Link className="navlink" to="/homeabout">
                 <p>About</p>
               </Link>
-              <Link className="navlink" to="/contact">
+              <Link className="navlink" to="/homecontact">
                 <p>Contact</p>
               </Link>
               <Link
