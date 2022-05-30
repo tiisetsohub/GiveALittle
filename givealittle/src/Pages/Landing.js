bc51b06bf6d36bc463617a9d908ab6030a4b6ea9
Thulasizwe <israelsabela@gmail.com>
2022-05-30 13:23:15 +0200

    Fixed duration

--- givealittle/src/Pages/Landing.js
+++ givealittle/src/Pages/Landing.js
@@ -827,7 +827,7 @@ export default function Landing() {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
-      exit={({ opacity: 0 }, { duration: 5 })}
+      exit={({ opacity: 0 }, { duration: 0.5 })}
     >
       <Navbar />
       <SearchSuggestion
