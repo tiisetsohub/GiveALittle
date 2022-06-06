643ca1a42af54de1a7efaf6f77ae698a057b321c
tiisetso <tiimojalefa@gmail.com>
2022-06-06 02:05:19 +0200

    Update allfunctions.test.js

--- allfunctions.test.js
+++ allfunctions.test.js
@@ -12,6 +12,15 @@ test("checks for valid login details", () => {
 test("checks for valid login details", () => {
     expect(login("tii", "234","234")).toBe(false)
 })
+test("checks for valid login details", () => {
+    expect(login("tii@gmail.com", "pass", "pass")).toBe("Password too short")
+})
+test("checks for valid login details", () => {
+    expect(login("tiiset", "password", "password")).toBe("Email too short")
+})
+test("checks for valid login details", () => {
+    expect(login("tii@gmail.com", "password", "passwordd")).toBe("Passwords do not match")
+})
 
 //tests for signup functions
 test("checks for valid signup details", () => {
@@ -19,6 +28,15 @@ test("checks for valid signup details", () => {
 })
 test("checks for valid signup details", () => {
     expect(signup("david","9044405950495059","tiisetso@gmail.com", "2344","2344")).toBe("Number invalid")
+})
+test("checks for valid signup details", () => {
+    expect(signup("tii", "0855853845", "tiiset", "password", "password")).toBe("Email too short")
+})
+test("checks for valid signup details", () => {
+    expect(signup("tii", "0855853845", "tii@gmail.com", "pass", "pass")).toBe("Password too short")
+})
+test("checks for valid signup details", () => {
+    expect(signup("ti", "0855853845", "tii@gmail.com", "password", "password")).toBe("Name too short")
 })
 
 //tests for card functions
